6433a058daa97c06beca96fb22b4c9b4bc027156
NicholasPeara92 <nicholas.peara@gmail.com>
2023-01-18 11:02:11 +0100

    Fix filter

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use App\Models\Train;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class PageController extends Controller
 {
   public function index()
   {
-    $trains = Train::whereDate('departure_time', date('y-m-d'))->get();
+    $trains = Train::whereDate('departure_time', '>=', Carbon::now())->get();
     
     return view('homepage', compact('trains'));
   }
